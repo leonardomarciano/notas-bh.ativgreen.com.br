9cef9f917be3116908d47b4368c4e20d1b299220
Leonardo Marciano <leonardo@sudobank.com>
2021-02-23 22:06:07 -0300

    up

--- api/EnvioLoteRPS/EnvioLoteRPS.js
+++ api/EnvioLoteRPS/EnvioLoteRPS.js
@@ -9,7 +9,7 @@ module.exports = {
     send: function (data, cb) {
 
         var request, fs, config, util, cert, key, messageTemplate, envelopeTemplate, 
-            envelope, xml, options;
+            envelope, options;
 
         request = require('request');
         fs = require('fs');
@@ -26,24 +26,73 @@ module.exports = {
         data.cnpjRemetente = config.cnpj;
 
         // Add sign and Incricao Municipal.
-        console.log(data)   
-            data.incricaoPrestador = config.incricaoMunicipal;
-            data.assinatura = util.buildRPSSign(key, data);
+        data.rps.forEach(function (rps) {
+            rps.incricaoPrestador = config.incricaoMunicipal;
+            rps.assinatura = util.buildRPSSign(key, rps);
+        });
 
         // Build envelope.
         envelope = util.buildEnvelope(cert, key, data, messageTemplate, envelopeTemplate);
-        
-       cb(envelope)
 
+        // Build request options.
+        options = util.buildRequestOptions(cert, key, envelope);
 
-        // // Build request options.
-        // options = util.buildRequestOptions(cert, key, envelope);
-
-        // // Send response.
+         cb(envelope)
+        // Send response.
         // request(options, function (error, response, body) {
         //     util.handleResponse(error, body, cb, data.attachments);
         // });
-
-      
     }
 };
+
+
+
+// module.exports = {
+
+//     /**
+//      * Send Lote RPS.
+//      *
+//      * @param {object} data
+//      * @param {function} cb
+//      */
+//     send: function (data, cb) {
+
+//         var request, fs, config, util, cert, key, messageTemplate, envelopeTemplate, 
+//             envelope, xml, options;
+
+//         request = require('request');
+//         fs = require('fs');
+//         util = require(__dirname + '/../util.js');
+
+//         config = util.getConfig();
+        
+//         cert = fs.readFileSync(config.certPath);
+//         key = util.readPrivateKeyFromProtectedPem(config.keyPath, 'fv100954');
+//         messageTemplate = fs.readFileSync(__dirname + '/message.xml', 'utf8');
+//         envelopeTemplate = fs.readFileSync(__dirname + '/envelope.xml', 'utf8');
+
+//         // Fixed data.
+//         data.cnpjRemetente = config.cnpj;
+
+//         // Add sign and Incricao Municipal.
+//         console.log(data)   
+//             data.incricaoPrestador = config.incricaoMunicipal;
+//             data.assinatura = util.buildRPSSign(key, data);
+
+//         // Build envelope.
+//         envelope = util.buildEnvelope(cert, key, data, messageTemplate, envelopeTemplate);
+        
+//        cb(envelope)
+
+
+//         // // Build request options.
+//         // options = util.buildRequestOptions(cert, key, envelope);
+
+//         // // Send response.
+//         // request(options, function (error, response, body) {
+//         //     util.handleResponse(error, body, cb, data.attachments);
+//         // });
+
+      
+//     }
+// };
